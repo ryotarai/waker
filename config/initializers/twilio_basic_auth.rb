6f91ca8fc207a5f9c3e7d3657a3b617475a62692
Ryota Arai <ryota.arai@gmail.com>
2014-07-28 19:40:31 +0900

    Do not generate username and pass for basic auth.

--- config/initializers/twilio_basic_auth.rb
+++
@@ -1,7 +0,0 @@
-require 'securerandom'
-
-ENV['TWILIO_BASIC_AUTH_USER'] ||= SecureRandom.hex
-ENV['TWILIO_BASIC_AUTH_PASSWORD'] ||= SecureRandom.hex
-
-Rails.logger.info "twilio basic auth user: #{ENV['TWILIO_BASIC_AUTH_USER']}"
-Rails.logger.info "twilio basic auth pass: #{ENV['TWILIO_BASIC_AUTH_PASSWORD']}"
