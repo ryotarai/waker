023b54ea9eab67279cffe74f7fe42efb52fa367e
Ryota Arai <ryota.arai@gmail.com>
2015-02-02 19:06:42 +0900

    Listen :8080 always.

--- docker/puma.rb
+++ docker/puma.rb
@@ -3,10 +3,12 @@ require 'fileutils'
 FileUtils.mkdir_p('/log')
 stdout_redirect '/log/puma-stdout.log', '/log/puma-stdout.log'
 
-if listen_unix = ENV['LISTEN_UNIX']
+listen_unix = ENV['LISTEN_UNIX']
+
+if listen_unix
   bind "unix://#{listen_unix}"
-else
-  port = ENV['PORT'] || 8080
-  bind "tcp://0.0.0.0:#{port}"
 end
+
+port = ENV['PORT'] || 8080
+bind "tcp://0.0.0.0:#{port}"
 
