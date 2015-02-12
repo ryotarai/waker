890c5b3a3a137202c6c4dedc62dc9453e9a7459a
Ryota Arai <ryota.arai@gmail.com>
2015-02-12 13:49:15 +0900

    TIME_ZONE option.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ module Waker
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    if time_zone = ENV['TIME_ZONE']
+      config.time_zone = time_zone
+    end
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
