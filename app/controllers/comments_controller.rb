573cfdc50347977fd4316ea3f7ffb62ad36f9e16
winebarrel <sgwr_dts@yahoo.co.jp>
2016-09-09 00:41:33 +0900

    Fix comments index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.where(incident: @incident)
   end
 
   # GET /comments/1
