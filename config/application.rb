ae4463cf944f6635006040d1671fa1412f635f0d
pyama86 <pyama@pepabo.com>
2020-01-17 22:22:39 +0900

    raise_in_transactional_callbacks is deprecated

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,6 @@ module Waker
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.middleware.use Rack::Health
   end
 end
