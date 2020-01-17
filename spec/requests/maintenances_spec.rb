e7f7e24bc10325064dd78feca7e416b09e8e3c0f
pyama86 <pyama@pepabo.com>
2020-01-17 23:14:35 +0900

    fix spec

--- spec/requests/maintenances_spec.rb
+++ spec/requests/maintenances_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Maintenances", type: :request do
   describe "GET /maintenances" do
     it "works! (now write some real specs)" do
       get maintenances_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
