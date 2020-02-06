9a77bfdc5ac28d303d31fd665c1bfc592e856310
pyama86 <pyama@pepabo.com>
2020-02-06 09:43:52 +0900

    set language

--- app/controllers/incident_events_controller.rb
+++ app/controllers/incident_events_controller.rb
@@ -5,6 +5,7 @@ class IncidentEventsController < ApplicationController
 
   def twilio
     @event = IncidentEvent.find(params[:id])
+    language = ENV['TWILIO_LANGUAGE'] || 'en-US'
     resp = nil
 
     if params[:Digits]
@@ -15,16 +16,16 @@ class IncidentEventsController < ApplicationController
         @event.incident.resolve! rescue nil
       end
       resp = Twilio::TwiML::VoiceResponse.new do |r|
-        r.Say @event.incident.status, voice: 'alice', language: 'en-US'
+        r.Say @event.incident.status, voice: 'alice', language: language
         r.Hangup
       end
     else
       resp = Twilio::TwiML::VoiceResponse.new do |r|
         r.gather timeout: 10, numDigits: 1 do |g|
-          g.say message: "This is Waker alert.", voice: 'alice', language: 'en-US'
-          g.say message: @event.incident.subject, voice: 'alice', language: 'en-US'
-          g.say message: "To acknowledge, press 1.", voice: 'alice', language: 'en-US'
-          g.say message: "To resolve, press 2.", voice: 'alice', language: 'en-US'
+          g.say message: "This is Waker alert.", voice: 'alice', language: language
+          g.say message: @event.incident.subject, voice: 'alice', language: language
+          g.say message: "To acknowledge, press 1.", voice: 'alice', language: language
+          g.say message: "To resolve, press 2.", voice: 'alice', language: language
         end
       end
     end
